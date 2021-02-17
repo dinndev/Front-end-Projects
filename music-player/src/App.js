4100a264085f1813a59696203072f6bb3057c92b
dinndev <dinndev@gmail.com>
2021-02-17 21:27:11 +0800

    React router setup

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,20 +1,30 @@
 import "./App.css";
 import Nav from "./Components/Nav";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import MusicPlayer from "./Components/MusicPlayer";
 import React from "react";
+import LikedSongs from "./Components/ LikedSongs";
+import Albums from "./Components/Albums";
 
 function App() {
   return (
     <div className="App flex h-full w-screen bg-gray-100">
       <Router>
-        <Switch>
-          <Nav />
-        </Switch>
+        <Nav />
         <div className="main-contents items-start w-full flex flex-col lg:h-screen h-full mx-6">
-          <Header />
-          <MusicPlayer />
+          <Switch>
+            <Route exact path="/">
+              <Header />
+              <MusicPlayer />
+            </Route>
+            <Route exact path="/liked">
+              <LikedSongs />
+            </Route>
+            <Route exact path="/albums">
+              <Albums />
+            </Route>
+          </Switch>
         </div>
       </Router>
     </div>
